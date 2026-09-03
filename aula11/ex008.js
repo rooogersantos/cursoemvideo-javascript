efc05486ab2d41300efdc0798fb3aa6d1fa5d9e5
Roger Patrick do Santos <roger.arqui2016@gmail.com>
2026-09-03 11:48:10 -0300

    Atualização dos exercícios do curso

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,1 +1,6 @@
-console.log('O console funcionou corretamente!')
+var velocidade = 20
+console.log(`A velocidade do seu caro é ${velocidade}km/h.`)
+if (velocidade > 60) {  //condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
