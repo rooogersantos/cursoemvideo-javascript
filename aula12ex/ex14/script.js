35e5a8f2582f1ef087505820134680be8cf758ca
Roger Patrick do Santos <roger.arqui2016@gmail.com>
2026-09-09 14:02:49 -0300

    Atualização dos exercícios do curso

--- aula12ex/ex14/script.js
+++ aula12ex/ex14/script.js
@@ -1,18 +1,23 @@
 function carregar () {
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
+    var img = window.document.getElementById('image')
     var data1 = new Date()
     var data2 = new Date()
     var hora1 = data1.getHours()
-    var hora2 = data1.getMinutes()
+    var hora2 = data2.getMinutes()
     msg.innerHTML = `Agora são ${hora1}:${hora2}.`
-    if (hora >= 6 && hora < 12) {
 
-    } else if (hora >= 12 && hora < 18) { 
-
-    } else if (hora >= 18 && hora < 24) {
-
+    if (hora1 >= 6 && hora1 < 12) {
+        img.src = 'manha.png'
+        document.body.style.background = '#dda610'
+    } else if (hora1 >= 12 && hora1 < 18) { 
+        img.src = 'tarde.png'
+        document.body.style.background = '#8aa70d'
+    } else if (hora1 >= 18 && hora1 < 24) {
+        img.src = 'noite.png'
+        document.body.style.background = '#10192e'
     } else {
-        
+        img.src = 'noite.png'
+        document.body.style.background = '#10192e'
     }
 }
