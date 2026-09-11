19ae7758f62e7fa67df28ca267a30fff84d25a22
Roger Patrick do Santos <roger.arqui2016@gmail.com>
2026-09-11 15:18:02 -0300

    Atualização dos exercícios

--- aula12ex/ex14/script.js
+++ aula12ex/ex14/script.js
@@ -1,19 +1,20 @@
-function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var formularioano = document.getElementById('txtano')
-    var result = document.querySelector('div#result')
-    if (formularioano.value.length == 0 || formularioano.value > ano) {
-        window.alert('[ERRO] Verifique os dados e tente novamente!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data1 = new Date()
+    var hora = data1.getHours()
+    var data2 = new Date()
+    var minutos = data2.getMinutes().toString().padStart(2, '0')
+    msg.innerHTML = `Agora são ${hora}:${minutos}.`
+    if (hora >= 6 && hora <12) {
+        //Bom dia!!
+        img.src = 'manha.png'
+        document.body.style.background = '#d6cc43'
+    } else if (hora >= 12 && hora <18){
+        img.src = 'tarde.png'
+        document.body.style.background = '#b9cc4a'
     } else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - formularioano.value
-        var genero = ''
-        if (fsex[0].checked){
-            genero = 'Homem'
-        } else if(fsex[1].checked) {
-            genero = 'Mulher'
-        }
-        result.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        img.src = 'noite.png'
+        document.body.style.background = '#0b1a46'
     }
 }
--- aula12ex/ex15/script.js
+++ aula12ex/ex15/script.js
@@ -1,1 +1,48 @@
-alert('Olá!')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var formularioano = document.getElementById('txtano')
+    var result = document.querySelector('div#result')
+    if (formularioano.value.length == 0 || formularioano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(formularioano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked){
+            genero = 'Homem'
+            if (idade >= 0 && idade < 2) {
+                img.setAttribute('src', 'homem-bebe.png')
+            } else if (idade < 12) {
+                img.setAttribute('src', 'homem-crianca.png')
+            } else if (idade < 20) {
+                img.setAttribute('src', 'homem-adolescente.png')
+            } else if (idade < 40) {
+                img.setAttribute('src', 'homem.png')
+            } else if (idade < 60) {
+                img.setAttribute('src', 'homem-meia-idade.png')
+            } else {
+                img.setAttribute('src', 'homem-idoso.png')
+            }
+        } else if(fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 2) {
+                img.setAttribute('src', 'mulher-bebe.png')
+            } else if (idade < 12) {
+                img.setAttribute('src', 'mulher-crianca.png')
+            } else if (idade < 20) {
+                img.setAttribute('src', 'mulher-adolescente.png')
+            } else if (idade < 40) {
+                img.setAttribute('src', 'mulher.png')
+            } else if (idade < 60) {
+                img.setAttribute('src', 'mulher-meia-idade.png')
+            } else {
+                img.setAttribute('src', 'mulher-idosa.png')
+            }
+        }
+        result.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        result.appendChild(img)
+    }
+}
