62fed5f0f45e994805899b363ce02d1cd6f3862f
Roger Patrick do Santos <roger.arqui2016@gmail.com>
2026-09-11 22:35:09 -0300

    Novos exercícios

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+var c = 1
+
+do {
+    console.log('Tudo bem?')
+    c++
+} while (c <= 10)
+
+/* while (c <= 5) {
+    console.log('Tudo bem?')
+    c++
+} */  
+
+/* console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?') */
