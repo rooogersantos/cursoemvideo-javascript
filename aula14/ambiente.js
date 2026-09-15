1410124509f50888c65da829208f2228d67bc488
Roger Patrick do Santos <roger.arqui2016@gmail.com>
2026-09-15 12:20:42 -0300

    Atualização dos exercícios

---
+++ aula14/ambiente.js
@@ -0,0 +1,19 @@
+var c = 1
+
+console.log('Vai começar...')
+for (c = 1; c <= 12; c++) {
+    console.log(c)
+}
+console.log('FIM!')
+
+/*do {
+    console.log(c)
+    c++
+} while (c <=5)
+
+while (c <=5) {
+    console.log(c)
+    c++
+}
+
+*/
